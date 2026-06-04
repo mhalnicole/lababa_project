520ae50c417c8e878098b7db430ca3f4fc07405a
Admin <stevenjantabungar@gmail.com>
2026-06-04 15:26:44 +0800

    Add RUD (Read, Update, Delete) for Employee management

--- operations/urls.py
+++ operations/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('addNewEmployee', views.addNewEmployee, name='addNewEmployee'),
+    path('viewEmployees', views.viewEmployees, name='viewEmployees'),
+    path('editEmployee/<str:employee_id>', views.editEmployee, name='editEmployee'),
+    path('deleteEmployee/<str:employee_id>', views.deleteEmployee, name='deleteEmployee'),
 ]
--- operations/views.py
+++ operations/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import EmployeeForm
+from .models import Employee
 
 @login_required
 def index(request):
@@ -12,7 +13,32 @@ def addNewEmployee(request):
         form = EmployeeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('viewEmployees')
     else:
         form = EmployeeForm()
     return render(request, 'addNewEmployee.html', {'form': form})
+
+@login_required
+def viewEmployees(request):
+    employees = Employee.objects.all()
+    return render(request, 'viewEmployees.html', {'employees': employees})
+
+@login_required
+def editEmployee(request, employee_id):
+    employee = get_object_or_404(Employee, pk=employee_id)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('viewEmployees')
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'editEmployee.html', {'form': form, 'employee': employee})
+
+@login_required
+def deleteEmployee(request, employee_id):
+    employee = get_object_or_404(Employee, pk=employee_id)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('viewEmployees')
+    return render(request, 'deleteEmployee.html', {'employee': employee})
